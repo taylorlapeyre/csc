00790b844d4c4c3182cbd1379128b7d60c3cb20e
Taylor Lapeyre <taylorlapeyre@gmail.com>
2014-04-21 13:19:19 -0500

    make code formatting consistent

--- 3501/homework4/cachesim.c
+++ 3501/homework4/cachesim.c
@@ -11,118 +11,118 @@ void GetFlags(int nargs, char **args, int *b, int *s, int *a, char *W){
     *s = 64;
     *a = 1;
     *W = 't';
-    for (i=1; i < nargs; i++)
-    {
-        if (args[i][0] == '-')
-        {
-                switch(args[i][1])
-            {
-                case 'b':
-                    //set the block size
-                    if(++i >= nargs)
-                        printf("Out of args. \n");
-                   //b should be a power of 2
-                    *b = atoi(args[i]);
-                    break;
-                case 's':
-                    //set the number of sets
-                    if(++i >= nargs)
-                        printf("Out of args");
-                    //s should be a power of 2 unless a is set to 0
-                    *s = atoi(args[i]);
-                    break;
-                case 'a':
-                    //set the associativity
-                    if(++i >= nargs)
-                        printf("Out of args");
-                    *a = atoi(args[i]);
-                    break;
-                case 'W':
-                    //set the write policy
-                    if(++i >= nargs)
-                        printf("Out of args");
-                    *W = args[i][0];
-                default:
-                    printf("something\n");
+    for (i=1; i < nargs; i++) {
+        if (args[i][0] == '-') {
+            switch(args[i][1]) {
+            case 'b':
+                // set the block size
+                if (++i >= nargs) {
+                    printf("Out of args. \n");
+                }
+               // b should be a power of 2
+                *b = atoi(args[i]);
+                break;
+            case 's':
+                // set the number of sets
+                if (++i >= nargs) {
+                    printf("Out of args");
+                }
+                // s should be a power of 2 unless a is set to 0
+                *s = atoi(args[i]);
+                break;
+            case 'a':
+                //set the associativity
+                if (++i >= nargs) {
+                    printf("Out of args");
+                }
+                *a = atoi(args[i]);
+                break;
+            case 'W':
+                //set the write policy
+                if (++i >= nargs) {
+                    printf("Out of args");
+                }
+                *W = args[i][0];
+            default:
+                printf("something went real wrong\n");
+                exit 1;
             }
-
         }
     }
-    //print the values for testing...
-    //printf("b:%d\ns:%d\na:%d\nW:%c\n",*b,*s,*a,*W);
-
 }
+
 //prints c num times
 void repeat(char c, int num){
-    int i;
-    for(i=0; i < num; i++)
+    for(int i = 0; i < num; i++) {
         printf("%c",c);
+    }
 }
 
 void PrintHeader(){
     printf("Hex");
-    repeat(' ',5);
+    repeat(' ', 5);
     printf("Binary Address");
-    repeat (' ', 23);
+    repeat (' ',  23);
     printf("Set  Blk");
-    repeat(' ',15);
+    repeat(' ', 15);
     printf("Memory\n");
 
-
     printf("Address (tag/index/offset)");
-    repeat(' ', 14);
+    repeat(' ',  14);
     printf("Tag Index off  Way UWay  Read Writ\n");
 
-    repeat('=',7);
+    repeat('=', 7);
     printf(" ");
-    repeat('=',26);
+    repeat('=', 26);
     printf(" ");
-    repeat('=',8);
+    repeat('=', 8);
     printf(" ");
-    repeat('=',5);
+    repeat('=', 5);
     printf(" ");
-    repeat('=',3);
+    repeat('=', 3);
     printf(" ");
-    repeat('=',4);
+    repeat('=', 4);
     printf(" ");
-    repeat('=',4);
+    repeat('=', 4);
     printf("  ");
-    repeat('=',4);
+    repeat('=', 4);
     printf(" ");
-    repeat('=',4);
+    repeat('=', 4);
     printf("\n");
 
 }
-#define NBITS 24
-char *Int2Bits(unsigned int bits)
+
 /*
  * RETURNS: string indicating binary bit pattern in interger bits
  */
+#define NBITS 24
+char *Int2Bits(unsigned int bits)
 {
-    static char cbits[NBITS+1]; /* NBITS macro defined elsewhere */
-    int i,j;
+    static char cbits[NBITS+1]; // NBITS macro defined elsewhere
     cbits[NBITS] = '\0';
-    for (i=0; i < NBITS; i++){
+    int i, j;
+
+    for (i = 0; i < NBITS; i++) {
         //needs to add spaces
         cbits[NBITS-1-i] = (bits == ((1<<i) | bits)) ? '1' : '0';
     }
-    static char fcbits[26+1];
+
+    static char fcbits[26 + 1];
     int tag,index;
     //HARDCODED:
     tag = 8 + 5;
     index = tag + 7;
-    for (i=0,j=0; i < 26; i++){
-        if(i==tag || i==index)
-        {
+    for (i = 0,j = 0; i < 26; i++){
+        if (i == tag || i == index) {
             fcbits[i] = ' ';
             continue;
         }
+
         fcbits[i] = cbits[j];
         j++;
-        //printf(":%c\n",fcbits[i]);
     }
-    return(fcbits);
 
+    return(fcbits);
 }
 
 
@@ -139,9 +139,11 @@ unsigned int GetMask(int nset)
     #ifdef FAST
     mask = (((long long)1))<<nset) - 1;
     #else
-    for (mask=i=0; i < nset; i++)
+    for (mask=i=0; i < nset; i++) {
         mask |= (1<<i);
+    }
     #endif
+
     return(mask);
 }
 
@@ -153,21 +155,29 @@ int main(int nargs, char **args)
 
     int b,s,a;
     static char W;
+
     GetFlags(nargs, args, &b, &s, &a, &W);
     PrintHeader();
-	//TAYLOR I NEED YOU TO WRITE THE HEADING HERE. it should look something like:
-	// 2KB 1-way associative cache: ...
+
+    //TAYLOR I NEED YOU TO WRITE THE HEADING HERE. it should look something like:
+    // 2KB 1-way associative cache: ...
+
     //number of addresses
     int nref = 0;
+
     //arrays containing the sets and tags of the input. size is initialized to the number of sets + 1
-    int sets[s+1];
-    int tags[s+1];
+    int sets[s + 1];
+    int tags[s + 1];
+
     //array holding the R/W bit for each row, will probablly not even need this array
     char readOrWrite[s+1];
+
     //holds the R/W bit to put into the array
     char RW;
+
     //printf("b:%d s:%d a:%d W:%c\n",b,s,a,W);
-    while(scanf("%[R|W]",&RW) == 1){
+
+    while(scanf("%[R|W]",&RW) == 1) {
         readOrWrite[nref] = RW;
         //printf("RW:%c\n",RW);
         //scan then skip colons
@@ -188,45 +198,50 @@ int main(int nargs, char **args)
         int writ = 0;
 
         //if the R/W bit is R, default read to 1, if W default writ to 1
-        if(RW == 'R') read = 1;
-        else if(RW == 'W' && W == 't') writ = 1;
-        else{}
+        if (RW == 'R') {
+          read = 1;
+        } else if (RW == 'W' && W == 't') {
+          writ = 1;
+        }
+
         int i;
-        if(nref > 0){
-            for(i = 0; i < nref; i++){
-                if(sets[i] == setIndex ){
-                    if(tags[i] == tag){
-                        if(RW == 'R'){
-                            way = 0;
+        if (nref > 0) {
+            for (i = 0; i < nref; i++) {
+                if (sets[i] == setIndex ) {
+                    if (tags[i] == tag) {
+                        if (RW == 'R') {
+                            way  = 0;
                             uway = -2;
                             read = 0;
-                        }else if(RW == 'W' && W == 'b'){
+                        } else if (RW == 'W' && W == 'b') {
                             way = 0;
                             uway = 0;
-                        }else{
+                        } else {
                             way = -1;
                             uway = -1;
                         }
+
                         break;
                     }
-                }else {
-                    if(i == nref-1){
+                } else {
+                    if (i == nref-1) {
                         //if the set is NOT found, add it to sets
                         sets[nref] = setIndex;
                         tags[nref] = tag;
-                        if(RW == 'W' && W == 't') uway = -1;
-                        else uway = 0;
+                        uway = (RW == 'W' && W == 't') ? -1 : 0;
                     }
                 }
             }
-        }else{
+        } else {
             sets[nref] = setIndex;
             tags[nref] = tag;
             //only works for 1-way associative
             uway = 0;
         }
         //terrible...
-        if(way == -1 && uway >= 0 && W == 'b') read = 1;
+        if (way == -1 && uway >= 0 && W == 'b')
+            read = 1;
+        }
 
         int blkOff = address & 31;
         //hardcoded
@@ -239,4 +254,3 @@ int main(int nargs, char **args)
     //printf("\n\n");
     return 0;
 }
-	
