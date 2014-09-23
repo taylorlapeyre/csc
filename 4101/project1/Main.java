de46ada2fcb5c892e9ab4b1b96cced1f88354017
Taylor Lapeyre <taylorlapeyre@gmail.com>
2014-09-23 09:44:45 -0500

    add a main class for the scheme parser

---
+++ 4101/project1/Main.java
@@ -0,0 +1,56 @@
+import java.io.*;
+
+public class Main {
+  public static final String TokenName[] = {
+    "QUOTE",
+    "LPAREN",
+    "RPAREN",
+    "DOT",
+    "TRUE",
+    "FALSE",
+    "INT",
+    "STRING",
+    "IDENT"
+  }
+
+
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+
+    if (args.length > 2) {
+      System.err.println("Usage: java Main " + "[-d]");
+      System.exit(1);
+    }
+
+    if (args.length == 2 && args[0].equals("-d")) {
+      Token token = scanner.getNextToken();
+      while (token) {
+        int tokenType = token.getType();
+        System.out.print(TokenName[tokenType]);
+
+        if (tokenType == Token.INT) {
+          System.out.println(", intVal = " + token.getIntVal());
+        } else if (tokenType == Token.STRING) {
+          System.out.println(", strVal = " + token.getStrVal());
+        } else if (tokenType == Token.IDENT) {
+          System.out.println(", name = " + token.getName());
+        } else {
+          System.out.println();
+        }
+
+        token = scanner.getNextToken();
+      }
+    }
+
+    Parser parser = new Parser(scanner);
+    Node root = parser.parseExp();
+
+    while (root) {
+      root.print();
+      root = parser.parseExp();
+    }
+
+    System.exit(0);
+  }
+
+}
