fdb5764b345c26f7cdd360b17cc0b6d31428c4c5
Taylor Lapeyre <taylorlapeyre@gmail.com>
2014-10-09 13:53:57 -0500

    large refactor of scanner

--- 4101/project1/Scanner.java
+++ 4101/project1/Scanner.java
@@ -1,164 +1,135 @@
-// Scanner.java -- the implementation of class Scanner
+package taylorlapeyre; /**
+ * Created by taylorlapeyre on 10/9/14.
+ */
 
 import java.io.*;
 
-class Scanner
-{
+public class Scanner {
     private PushbackInputStream in;
-    private byte[] buf = new byte[1000];
 
-    public Scanner(InputStream i) {
-        in = new PushbackInputStream(i);
+    public Scanner(InputStream input) {
+        in = new PushbackInputStream(input);
     }
 
-    public char readNextChar() {
+    public int readNextBite() {
         int bite = -1;
-
         try {
             bite = in.read();
         } catch (IOException e) {
-            System.err.println("We fail: " + e.getMessage());
+            System.err.println("Error getting input: " + e.getMessage());
         }
 
         return bite;
     }
 
     public Token getNextToken() {
-
-        char bite = readNextChar();
+        int bite = readNextBite();
 
         // Nothing
         if (bite == -1) {
             return null;
         }
 
-        char ch = (char) bite;
+        char ch = (char)bite;
+
 
         // Whitespace
-        if (ch == " " || ch == "\n") {
-            return getNextToken();
-        } else if (ch == ";") {
-            do {
-                bite = in.read();
-            } while (ch != "\n");
+        if (ch == ' ' || ch == '\n') {
             return getNextToken();
         }
 
-        // Special characters
-        if (ch == '\'') {
-            return new Token(Token.QUOTE);
-        } else if (ch == '(') {
-            return new Token(Token.LPAREN);
-        } else if (ch == ')') {
-            return new Token(Token.RPAREN);
-        } else if (ch == '.') {
-            return new Token(Token.DOT);
+
+        // Comments
+        if (ch == ';') {
+            while (ch != '\n') {
+                ch = (char)readNextBite();
+            }
+            return getNextToken();
         }
 
-        // Boolean constants
-        if (ch == '#') {
-            bite = readNextChar();
 
-            if (bite == -1) {
-                System.err.println("Unexpected EOF following #");
-                return null;
-            }
-
-            ch = (char) bite;
-
-            if (ch == 't') {
-                return new Token(Token.TRUE);
-            } else if (ch == 'f') {
-                return new Token(Token.FALSE);
-            } else {
-                System.err.println("Illegal character '" + (char) ch + "' following #");
-                return getNextToken();
-            }
-        }
-
-        // String constants
+        // Strings
         if (ch == '"') {
-            for (int i = 0; i < buf.length; i++) {
-                try {
-                    ch = (char) readNextChar();
-
-                    if (ch == "\"") {
-                        buf[i] = (byte) '\\';
-                        i++;
-                        ch = (char) in.read();
-                        buf[i] = (byte) ch;
-                        continue;
-                    }
-
-                    if (ch == '\"') {
-                        break;
-                    }
-                    buf[i] = (byte) ch;
-                } catch (IllegalArgumentException e) {
-                    System.err.println("Error, please try again: " + e.getMessage());
-                }
+            StringBuilder sb = new StringBuilder();
+            ch = (char)readNextBite();
+            while (ch != '"') {
+                sb.append(ch);
+                ch = (char)readNextBite();
             }
-            byte[] str = new byte[i];
-            for (int j = 0; j < i; j++) {
-                str[j] = buf[j];
-            }
-            return new StrToken(new String(str));
+            return new StringToken(sb.toString());
         }
 
 
-        // Integer constants
-        if (ch >= '0' && ch <= '9') {
-            int i = ch - '0';
-            int outcome = 0;
+        // Special Characters
+        // Quote
+        if (ch == '\'') {
+            return new Token("QUOTE");
+        }
 
-            do {
-                try {
-                    outcome = outcome * 10 + i;
-                    ch = (char) readNextChar();
-                    i = ch - '0';
-                } catch (IOException e) {
-                    System.err.println("Failed to create Integer constants: " + e.getMessage());
-                }
-            } while (ch >= '0' && ch <= '9');
+        // Left Parenthesis
+        if (ch == '(') {
+            return new Token("LPAREN");
+        }
+
+        // Right Parenthesis
+        if (ch == ')') {
+            return new Token("RPAREN");
+        }
+
+        if (ch == '.') {
+            return new Token("DOT");
+        }
+
+
+        // Boolean Constants
+        if (ch == '#') {
+            ch = (char)readNextBite();
+            if (ch == 't') {
+                return new Token("TRUE");
+            } else if (ch == 'f') {
+                return new Token("FALSE");
+            } else {
+                System.err.println("Invalid boolean constant");
+            }
+        }
+
+
+        // Integer Constants
+        if (ch >= '0' && ch <= '9') {
+            StringBuilder sb = new StringBuilder();
+            while (ch >= '0' && ch <= '9') {
+                sb.append(ch);
+                ch = (char)readNextBite();
+            }
 
             try {
                 in.unread((byte) ch);
-            } catch (IllegalArgumentException e) {
-                System.err.println("Error, please try again: " + e.getMessage());
+            } catch (java.io.IOException e) {
+                System.err.println("No character to unread!");
             }
 
-            return new IntToken(outcome);
+            return new IntToken(Integer.parseInt(sb.toString()));
         }
+
 
         // Identifiers
         if (ch >= 'A' && ch <= 'Z' || ch >= 'a' && ch <= 'z') {
-            buf[0] = (byte) ch;
-            int i = 1;
-
-            do {
-                try {
-                    bite = readNextChar();
-                    buf[i] = (byte) bite;
-                    ch = (char) bite;
-                    i++;
-                } catch (IllegalArgumentException e) {
-                    System.err.println("Error, please try again: " + e.getMessage());
-                }
-            } while (ch >= 'A' && ch <= 'Z' || ch >= 'a' && ch <= 'z');
+            StringBuilder sb = new StringBuilder();
+            while (ch >= 'A' && ch <= 'Z' || ch >= 'a' && ch <= 'z') {
+                sb.append(ch);
+                ch = (char)readNextBite();
+            }
 
             try {
                 in.unread((byte) ch);
-            } catch (IOException e) {
-                System.err.println("We Fail!!! " + e.getMessage());
-            } catch (IllegalArgumentException e) {
-                System.err.println("Error, please try again: " + e.getMessage());
+            } catch (java.io.IOException e) {
+                System.err.println("No character to unread!");
             }
 
-            return new IdentToken(buf.toString());
+            return new IdentToken(sb.toString());
         }
 
-        // Illegal character
         System.err.println("Illegal input character '" + (char) ch + '\'');
         return getNextToken();
     }
-};
+}
