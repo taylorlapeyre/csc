b6183f811f704e72c17ce186cd645875094038b5
Taylor Lapeyre <taylorlapeyre@gmail.com>
2014-11-13 11:02:21 -0600

    add regular eval

--- 4101/project2/Regular.java
+++ 4101/project2/Regular.java
@@ -19,4 +19,23 @@ class Regular extends Special
 	System.out.print(' ');
 	c.getCdr().print(0, true);
   }
+
+	public Node eval(Node t, Environment env) {
+		Node first = t.getCar();
+		Node rest  = t.getCdr();
+
+		if (first.isSymbol()) {
+			first = env.lookup(first);
+		}
+
+		if (first.isNull() || first == null) {
+			return null;
+		}
+
+		if (first.isProcedure()) {
+			return first.apply(rest, env);
+		} else {
+			return first.eval(env).apply(rest, env);
+		}
+	}
 }
